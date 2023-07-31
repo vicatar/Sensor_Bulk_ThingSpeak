bac3737531c5648aa6721306e785b714fbf7e3bb
Victor Lin <victor.s.lin@gmail.com>
2023-07-31 12:50:47 -0700

    Updated file header comment to help other understand the purpose, etc.

--- bulk_lth_thingspeak.ino
+++ bulk_lth_thingspeak.ino
@@ -1,4 +1,28 @@
-/* This example shows how to bulk update a ThingSpeak channel using an an ESP8266*/
+/*
+Author: Victor Lin
+Date: 2023-07-19
+Version: 1.0.0
+Description:
+  This code bulk update sensor data to a ThingSpeak channel using an an ESP8266. The code is basd on an Mathworks example.
+
+License:
+  GNU General Public License v3.0
+
+Dependencies:
+  - DHT library
+  - EPS8266Wifi library
+
+Usage Instructions:
+  1. Ensure you have DHT sensor connected to correct PINS on the ESP NodeMCU 1.0
+  2. Ensure you have a photocell sensor connected to correct PINS on the NodeMCU
+  2. Ensure you have set up a ThingSpeak account and created a channel
+  3. Replace the constants noted below with your ThingSpeak channel write API key & and ID
+  4. Compile and run this code on the NodeMCU to upload DHT and photocell sensor data to ThingSpeak
+
+References:
+  - https://www.mathworks.com/help/thingspeak/continuously-collect-data-and-bulk-update-a-thingspeak-channel-using-an-arduino-mkr1000-board-or-an-esp8266-board.html
+
+*/
 
 #include<ESP8266WiFi.h> 
 #include <DHT.h> 
